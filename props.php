56005c0772a83612c91dc744899333465449c2b1
BILL BRIAN <46356585+CocoGinger@users.noreply.github.com>
2019-11-21 20:29:34 +0300

    Create props.php

---
+++ props.php
@@ -0,0 +1,9 @@
+<?php
+/*Properties file to enable ease in modication
+*Set all properties in this file
+*/
+
+define(SERVER, 'localhost');
+define(USER, 'root');
+define(PASS, '');
+define(DB, 'registration');
